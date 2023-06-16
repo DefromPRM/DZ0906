4b76469933dd092e170e66e2698a033f837585e3
DefromPRM <defromprm@gmail.com>
2023-06-16 21:13:44 +0500

    DZ_09_06_23

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿// Задача 47. Задайте двумерный массив 
+// размером m×n, заполненный случайными 
+// вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// int rows = ReadInt("Введите количество строк: ");
+// int colums = ReadInt("Введите количество столбцов: ");
+// double[,] numbers = new double[rows, colums];
+// void FillArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-99, 99) / 10.0;
+//         }
+//     }
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// FillArray(numbers);
+// PrintArray(numbers);
+
+
+// Задача 50. Напишите программу, 
+// которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает 
+// значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// int rows = ReadInt("Введите индекс строки: ");
+// int colums = ReadInt("Введите индекс столбца: ");
+// int[,] numbers = new int[6, 8];
+// if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+// else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// FillArray(numbers);
+// PrintArray(numbers);
+
+
+// Задача 52. Задайте двумерный массив 
+// из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+// Console.Write("Введите колличество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите колличество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] numbers = new int[m, n];
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+// void AvgColumns(int[,] array)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         double Result = 0;
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             Result += array[i, j];
+//         }
+//         Console.Write($"{Result / array.GetLength(0)}; ");
+//     }
+// }
+
+// FillArray(numbers);
+// PrintArray(numbers);
+// Console.WriteLine($"");
+// AvgColumns(numbers);
